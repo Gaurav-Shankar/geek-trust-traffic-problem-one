f85b135fa78a939cfe7de828cd3aeceab299b7f8
Koushik Muthakana <kmuthakana@gmail.com>
2018-05-09 12:57:25 +0530

    Updated with Comments

--- Problem_one.py
+++ Problem_one.py
@@ -1,4 +1,14 @@
+'''
+problem 1: lengaburu traffic
+King Shan wants to visit the suburb of Hallitharam, and has 2 possible orbits and 3 possible vehicles to chose from.
+Your coding challenge is to determine which orbit and vehicle King Shan should take to reach Hallitharam the fastest
+'''
+
 def getvehicles(climate):
+    '''
+       :param climate: type of climate
+       :return: Based on climete, return available vehicles
+       '''
     Bike = {'name':'Bike','speed_in_Km': 10, 'crater_speed_in_Min': 2}
     Tuktuk = {'name':'Tuktuk','speed_in_Km': 12, 'crater_speed_in_Min': 1}
     Car = {'name':'Car','speed_in_Km': 20, 'crater_speed_in_Min': 3}
@@ -10,6 +20,14 @@ def getvehicles(climate):
         return [[Car,Bike],0.0]
 
 def get_orbit_Time(vehicles,taffic_speed,orbit_distance,craters_count):
+    '''
+
+     :param vehicles: Based on Climate, gets the available vehicles
+     :param taffic_speed: orbit traffic speed
+     :param orbit_distance: orbit distance
+     :param craters_count: Number of craters in particular orbit
+     :return: [Vehicle Details,minimum Time takes to travel in Minutes, Craters change ]
+     '''
     orbit_1_vehicles_time=[]
     for i in vehicles[0]:
         if taffic_speed <= i['speed_in_Km']:
@@ -20,20 +38,26 @@ def get_orbit_Time(vehicles,taffic_speed,orbit_distance,craters_count):
         orbit_1_vehicles_time.append(temp)
 
     return [vehicles[0][orbit_1_vehicles_time.index(min(orbit_1_vehicles_time))],min(orbit_1_vehicles_time)]
-def getMinPath(climate,taffic_speed_obit1,taffic_speed_obit2):
+def getMinPath(climate,traffic_speed_orbit1,traffic_speed_orbit2):
+    '''
+      :param climate: User input  CLimate
+      :param traffic_speed_orbit1: Orbit 1 Traffic Speed
+      :param traffic_speed_orbit2: Orbit 2 Traffic Speed
+      :return: Shortest time and Vehicle details
+      '''
     print("Weather is  ", climate)
-    print("Orbit1 traffic speed is {0} megamiles/hour".format(taffic_speed_obit1))
-    print("Orbit1 traffic speed is {0} megamiles/hour".format(taffic_speed_obit2))
+    print("Orbit1 traffic speed is {0} megamiles/hour".format(traffic_speed_orbit1))
+    print("Orbit1 traffic speed is {0} megamiles/hour".format(traffic_speed_orbit2))
     vehicles=getvehicles(climate)
-    orbit1=get_orbit_Time(vehicles,taffic_speed_obit1,18,20)
-    orbit2 = get_orbit_Time(vehicles, taffic_speed_obit2, 20, 10)
+    orbit1 = get_orbit_Time(vehicles=vehicles, taffic_speed=traffic_speed_orbit1, orbit_distance=18, craters_count=20)
+    orbit2 = get_orbit_Time(vehicles=vehicles, taffic_speed=traffic_speed_orbit2, orbit_distance=20, craters_count=10)
     if orbit1[1] < orbit2[1]:
         print("Vehicle {0} on Orbit1".format(orbit1[0]['name']))
     else:
         print("Vehicle {0} on Orbit2".format(orbit2[0]['name']))
 
 
-getMinPath(climate="Sunny",taffic_speed_obit1=12,taffic_speed_obit2=10)
+getMinPath(climate="Sunny",traffic_speed_orbit1=12,traffic_speed_orbit2=10)
 
 
 
